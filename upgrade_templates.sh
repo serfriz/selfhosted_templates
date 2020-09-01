03ac65c58b9fab38966f8a50518c9bd1ad16dc8d
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-09-01 14:06:03 +0200

    Updated pihole template

--- upgrade_templates.sh
+++ upgrade_templates.sh
@@ -1,6 +1,9 @@
 # Convert the templates from 1.0 to 2.0
 # Taken from https://github.com/portainer/helper-templates
 
+# Original is:
+# https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json
+
 # specify the path to your original template.json file
 # (take care of the wording here!)
 docker run --rm \
@@ -9,4 +12,4 @@ docker run --rm \
     portainer/helper-templates
 
 # Rename
-mv Template2_0/templates_upgraded.json Template2_0/templates.json
+mv Template2_0/templates-upgraded.json Template2_0/templates.json
